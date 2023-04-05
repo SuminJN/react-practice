54f07b2395430683fca795fb2860c57cc0ab4c60
SuminJN <wjstnals1211@gmail.com>
2023-04-05 17:13:57 +0900

    버튼추가

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,16 @@
-function Button({text}) {
-    return <button>{text}</button>;
+import propTypes from "prop-types";
+import styles from "./Button.module.css"
+
+function Button({ text }) {
+    return (
+        <button className={styles.btn}>
+            {text}
+        </button>
+    );
+}
+
+Button.prototype = {
+    text: propTypes.string.isRequired,
 }
 
 export default Button;
