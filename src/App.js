9df59d48ff896f6412406bdf2ea6146695b289c2
SuminJN <wjstnals1211@gmail.com>
2023-04-12 13:55:58 +0900

    오타 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
